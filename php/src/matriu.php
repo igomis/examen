a5d202294b4920e4ef036a773440af856e2e48bb
igomis <igomis@cipfpbatoi.es>
2024-11-13 10:49:04 +0100

    first commit

--- php/src/matriu.php
+++ php/src/matriu.php
@@ -2,7 +2,7 @@
 <body>
 <form action="matriu.php" method="post">
     <label for="numero">Introdueix un número:</label>
-    <input type="number" id="numero" name="numero" min="1" max="10">
+    <input type="number" id="numero" name="numero" >
     <input type="submit" value="Enviar">
 </form>
 </body>
