900c590a9532e09662bed6208301636f2f7655be
igomis <igomis@cipfpbatoi.es>
2024-11-19 12:41:49 +0100

    RA5 i RA6

---
+++ php/src/inici.php
@@ -0,0 +1,15 @@
+<?php
+    $idiomas = ['va' => 'Valencià', 'es' => 'Castellano', 'en' => 'English'];
+?>
+<html>
+<head>
+    <title>Formulari d'inici</title>
+</head>
+<body>
+    <form method="POST" action="inici.php">
+        <select name="idioma">
+
+        </select>
+    </form>
+</body>
+</html>
